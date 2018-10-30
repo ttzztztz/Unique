63ab0c5eb69c8e544e9ab5c3603e46760f96e140
Rabbit <ttzztztz@outlook.com>
2018-10-30 13:05:46 +0800

    MERGE SORT FIX

--- 归并数组C版.cpp
+++ 归并数组C版.cpp
@@ -1,35 +1,36 @@
 #include<iostream>
 using namespace std;
-void merge(int* arr,int size1,int size2) {
+void merge(int* arr, int size1, int size2) {
 	int index1 = 0, index2 = 0;
-	int* temp = new int[size1+size2];
+	int* temp = new int[size1 + size2];
 	while (index1 + index2 < size1 + size2) {
 		//谁小谁先上
 		if (index1 < size1 && (index2 >= size2 || arr[index1] <= arr[size1 + index2])) {
 			temp[index1 + index2] = arr[index1++];
 		}
 		if (index2 < size2 && (index1 >= size1 || arr[index1] >= arr[size1 + index2])) {
-			temp[index1 + index2] = arr[size1+index2++];
+			temp[index1 + index2] = arr[size1 + index2++];
 		}
 	}
 	//拷贝数组
 	for (int i = 0; i < size1 + size2; i++) {
 		arr[i] = temp[i];
 	}
+	delete[] temp;
 }
-void sort(int*arr , int size) {
+void sort(int*arr, int size) {
 	if (size <= 1) return;
 	int size1 = size / 2; int size2 = size - size1;
 	sort(arr, size1);
-	sort(arr,size2);
-	merge(arr,size1,size2);
+	sort(arr+size1, size2);
+	merge(arr, size1, size2);
 }
 int main() {
-	int arr[] = { 4,5,1,2,8,9,6,21 };
+	int arr[] = { 4,5,1,2,9,666,8,6,21 };
 	int len = sizeof(arr) / sizeof(int);
-	sort(arr,len);
+	sort(arr, len);
 
-	for (int i : arr) cout << i<<endl;
+	for (int i : arr) cout << i << endl;
 
 	return 0;
 }
